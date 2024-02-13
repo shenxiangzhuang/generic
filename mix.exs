6fb686d051b4033eede04d67acb97c2f82e5a957
Xiangzhuang Shen <datahonor@gmail.com>
2024-02-13 12:07:43 +0800

    add: docs build

--- mix.exs
+++ mix.exs
@@ -7,7 +7,16 @@ defmodule Geneticx.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "Genetic algorithms",
+      source_url: "https://github.com/shenxiangzhuang/geneticx",
+      homepage_url: "https://github.com/shenxiangzhuang/geneticx",
+      docs: [
+        main: "Geneticx", # The main page in the docs
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -21,6 +30,7 @@ defmodule Geneticx.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
