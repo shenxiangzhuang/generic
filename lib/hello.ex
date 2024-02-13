699b1b3353c10d7c8ec9fca5bad870b1d28867ec
Xiangzhuang Shen <datahonor@gmail.com>
2024-02-13 18:02:59 +0800

    chore

---
+++ lib/hello.ex
@@ -0,0 +1,17 @@
+defmodule Hello do
+  @moduledoc """
+  Hello Module.
+  """
+
+  @doc """
+  Hello, World.
+
+  ## Examples
+
+    iex> Geneticx.hello()
+    :world
+  """
+  def hello do
+    :world
+  end
+end
--- test/geneticx_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule GeneticTest do
-  use ExUnit.Case
-  doctest Geneticx
-
-  test "greets the world" do
-    assert Geneticx.hello() == :world
-  end
-end
